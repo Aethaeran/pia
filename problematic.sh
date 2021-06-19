84706b74b993c8f884261cb067e63deb053badc8
Aethaeran <33813964+Aethaeran@users.noreply.github.com>
2021-06-19 16:06:07 -0700

    Moved "problematic" code back in
    You know, since it wasn't actually the problem.

--- problematic.sh
+++ problematic.sh
@@ -1,15 +1,2 @@
 
 
-
-
-echo
-echo "${green}Flush current iptables rules${devoid}"
-sudo iptables -F
-sudo iptables -X
-echo "${green}Add rule, which will block vpn user’s access to Internet (except the loopback device).${devoid}"
-sudo iptables -A OUTPUT ! -o lo -m owner --uid-owner vpn -j DROP
-echo "${green}Install iptables-persistent to save this single rule that will be always applied on each system start.${devoid}"
-echo
-echo "${yellow}During the install, iptables-persistent will ask you to save current iptables rules to /etc/iptables/rules.v4, accept this with YES.${devoid}"
-echo
-sudo apt-get install iptables-persistent -y
--- split_tunnel_VPN.sh
+++ split_tunnel_VPN.sh
@@ -152,6 +152,20 @@ sudo sed -i -e "s/#     foreign_option_3='dhcp-option DOMAIN be.bnc.ch'/foreign_
 echo
 
 
+
+echo
+echo "${green}Flush current iptables rules${devoid}"
+sudo iptables -F
+sudo iptables -X
+echo "${green}Add rule, which will block vpn user’s access to Internet (except the loopback device).${devoid}"
+sudo iptables -A OUTPUT ! -o lo -m owner --uid-owner vpn -j DROP
+echo "${green}Install iptables-persistent to save this single rule that will be always applied on each system start.${devoid}"
+echo
+echo "${yellow}During the install, iptables-persistent will ask you to save current iptables rules to /etc/iptables/rules.v4, accept this with YES.${devoid}"
+echo
+sudo apt-get install iptables-persistent -y
+
+
 sudo systemctl start openvpn@openvpn
 
 exit 0
